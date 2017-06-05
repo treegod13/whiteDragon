c29b42cc972ba0cfe5c9e334a5e48ca7d06ed62d
chenmz <mzchen1991@foxmail.com>
2017-06-05 06:12:14 +0000

    add 1 file, revise 1 file

--- controller.py
+++ controller.py
@@ -1,39 +1,49 @@
 import RPi.GPIO as GPIO
 import time
 
-forward_pin = [18, 24]
-backward_pin = [23, 25]
-left_pin = 18
-right_pin = 24
+class controller(object):
+    forward_pin = [18, 24]
+    backward_pin = [23, 25]
+    left_pin = 18
+    right_pin = 24
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(forward_pin, GPIO.OUT)
-GPIO.setup(backward_pin, GPIO.OUT)
+    def __init__(self):
+        pass
 
-def move_forward(d):
-    GPIO.output(forward_pin, True)
-    t = d/0.6
-    time.sleep(t)
-    GPIO.output(forward_pin, False)
+    def setup(self):
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.forward_pin, GPIO.OUT)
+        GPIO.setup(self.backward_pin, GPIO.OUT)
 
-def move_backward(d):
-    GPIO.output(backward_pin, True)
-    t = d/0.6
-    time.sleep(t)
-    GPIO.output(backward_pin, False)
+    def move_forward(self, d):
+        GPIO.output(self.forward_pin, True)
+        t = d/0.6
+        time.sleep(t)
+        GPIO.output(self.forward_pin, False)
 
-def turn_left(a):
-    GPIO.output(left_pin, True)
-    t = a / 0.81
-    time.sleep(t)
-    GPIO.output(left_pin, False)
+    def move_backward(self, d):
+        GPIO.output(self.backward_pin, True)
+        t = d/0.6
+        time.sleep(t)
+        GPIO.output(self.backward_pin, False)
 
-def turn_right(a):
-    GPIO.output(right_pin, True)
-    t = a / 0.81
-    time.sleep(t)
-    GPIO.output(right_pin, False)
+    def turn_left(self, a):
+        GPIO.output(self.left_pin, True)
+        t = a / 0.81
+        time.sleep(t)
+        GPIO.output(self.left_pin, False)
 
-def stop():
-    GPIO.output(forward_pin + backward_pin, False)
+    def turn_right(self, a):
+        GPIO.output(self.right_pin, True)
+        t = a / 0.81
+        time.sleep(t)
+        GPIO.output(self.right_pin, False)
+
+    def stop(self):
+        GPIO.output(self.forward_pin + self.backward_pin, False)
+
+    def clear(self):
+        GPIO.cleanup()
+
+
