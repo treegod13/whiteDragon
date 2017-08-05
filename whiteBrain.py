9f3f2aad35104591cdedc60f370aa1a02efa5a55
chenmz <mzchen1991@foxmail.com>
2017-08-05 02:17:49 +0000

    add 2 files

--- whiteBrain.py
+++ whiteBrain.py
@@ -1,13 +1,49 @@
-from transition import Machine
-import random
+import sm
+import io
+import math
 
-class whiteDragon(object)
-    state = ['sleep', 'hanging out', 'hungry', 'sweaty', 'saving the world']
+# Move forward for 1 meter
+class ForwardTSM(sm.SM):
+    startState = (0, 0, 0)
+    def getNextValues(self, state, inp):
+        print inp
+        (last_x, last_y, last_theta) = state
+        new_x = last_x + math.cos(last_theta)
+        new_y = last_y + math.sin(last_theta)
+        new_state = (new_x, new_y, last_theta)
+        print new_state
+        return (new_state, controller.action(0, 1)) # angle:0 distance:1
 
-    def __init__(self, name):
-        self.name = name
-        self.cat_rescued = 0
-        self.machine = Machine(model = self, states =whiteDragon.states,  )
+# Rotate
+class RotateTSM(sm.SM)
+
+# Create instance of White
+class brain():
+    def __init__(self):
+        self.behavior = ForwardTSM()
+        self.controller = io.Controller()
+        self.senser = io.Senser()
+
+    def setup(self):
+        self.senser.setup()
+        self.controller.setup()
+        self.behavior.start()
+
+# Create instance of WhiteDragon
+behavior = ForwardTSM()
+senser = io.Senser()
+controller = io.Controller()
+
+def setup():
+    senser.setup()
+    controller.setup()
+    behavior.start()
+
+def step():
+    behavior.step(senser.sensorInput())
+
+
+
 
 
 
