f87aaee931fb240fae53800f0e32f0cdc6a00612
chenmz <mzchen1991@foxmail.com>
2017-08-15 09:14:26 +0000

    add FindWall class

--- whiteBrain.py
+++ whiteBrain.py
@@ -2,7 +2,7 @@ import sm
 import io
 import math
 
-# Move forward for 1 meter
+# Move forward for 0.01 meter
 class ForwardTSM(sm.SM):
     startState = (0, 0, 0)
     def getNextValues(self, state, inp):
@@ -12,37 +12,60 @@ class ForwardTSM(sm.SM):
         new_y = last_y + math.sin(last_theta)
         new_state = (new_x, new_y, last_theta)
         print new_state
-        return (new_state, controller.action(0, 1)) # angle:0 distance:1
+        return (new_state, white.controller.action(0, 0.01)) # angle:0 distance:1
 
 # Rotate
-class RotateTSM(sm.SM)
+#class RotateTSM(sm.SM)
 
-# Create instance of White
-class brain():
+# Stop
+class StopSM(sm.SM):
+    startState = (0, 0, 0)
+    def getNextValues(self, state, inp):
+        return (state, white.controller.action(0,0))
+
+# Wall finder
+# SenserInput:(distance(unit: mm), barrierLeft(value:0, 1), barrierLeft(value:0, 1))
+class WallFinder(sm.SM):
+    startState = 0
+    def __init__(self, dDesired):
+        self.dD = dDesired
+    def getNextValues(self, state, inp):
+        print state, inp
+        dN = inp[0]/100
+        dM = dN - self.dD
+        if dM < 0.01:
+            return (state, white.controller.action(0, 0))
+        elif dM < 1:
+            return (state + dM/100, white.controller.action(0, dM/100))
+        else:
+            return (state + dM/10, white.controller.action(0, dM/10))
+
+# WhiteBrain
+class Brain(object):
     def __init__(self):
-        self.behavior = ForwardTSM()
+        self.behavior = StopSM()
         self.controller = io.Controller()
         self.senser = io.Senser()
-
     def setup(self):
         self.senser.setup()
         self.controller.setup()
         self.behavior.start()
+    def step(self):
+        self.behavior.step(self.senser.sensorInput())
+    def run(self):
+        i = 0
+        while(1):
+            i = i + 1
+            self.step()
+    def stop(self):
+        self.controller.stop()
 
 # Create instance of WhiteDragon
-behavior = ForwardTSM()
-senser = io.Senser()
-controller = io.Controller()
-
-def setup():
-    senser.setup()
-    controller.setup()
-    behavior.start()
-
-def step():
-    behavior.step(senser.sensorInput())
-
-
+white = Brain()
+white.behavior = WallFinder(0.1)
+white.setup()
+#white.step()
+white.run()
 
 
 
