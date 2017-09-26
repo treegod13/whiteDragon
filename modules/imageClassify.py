fdf2f6f73d39339fc411f719d021b3665d0f1908
chenmz <mzchen1991@foxmail.com>
2017-09-26 08:20:15 +0000

    add the image classification function

--- modules/imageClassify.py
+++ modules/imageClassify.py
@@ -3,6 +3,7 @@ from picamera.array import PiRGBArray
 from picamera import PiCamera
 #import classify_image as cnn
 import tensorflow as tf
+import numpy as np
 import argparse
 import time
 import cv2
@@ -22,23 +23,38 @@ camera = PiCamera()
 rawCapture = PiRGBArray(camera)
 
 # Load class labels
-rows = open(conf['Label_path'])
+rows = open(conf['Caffe_label']).read().split('\n') # open():retrun file handle. read():return strings of file. split():return list of strings
+classes = [r[r.find(' ')+1:] for r in rows]
 
 # warmup camera
+print 'Warming the camera...'
 time.sleep(conf['camera_warmup_time'])
 
 # Capture the image and change into blob
 camera.capture(rawCapture, format="bgr")
 image_array = rawCapture.array
-image = cv2.dnn.blobFromImage(image_array, 1, (224, 224), (104, 117, 123))
-
+blob = cv2.dnn.blobFromImage(image_array, 1, (224, 224), (104, 117, 123))
 #cnn.run_inference_on_image(image)
 
 # Load model
-network = cv2.dnn.
+print 'Loading model...'
+network = cv2.dnn.readNetFromCaffe(conf['Caffe_prototxt'], conf['Caffe_model'])
 
+# Predict the label
+print 'Predicting label...'
+network.setInput(blob)
+start = time.time()
+scores = network.forward()
+end =time.time()
+print 'Use time:' + str(end-start) + ' seconds.'
+indexs = np.argsort(scores[0])[::-1][:5]  # [::-1]:reverse the array
 
+# Show the results
+print 'The results are:'
+for (i, index) in enumerate(indexs):
+    if i == 0:
+        cv2.putText(image_array, 'Object is: {}'.format(classes[index]), (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+    print 'No.{}: {} . Scores: {}'.format(i+1, classes[index], scores[0][index])
 
-
-#cv2.imshow("image", image)
-#cv2.waitKey(0)
+cv2.imshow("image", image_array)
+cv2.waitKey(0)
